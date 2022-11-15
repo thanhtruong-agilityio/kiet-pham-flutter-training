7f3a0f12009568a31ce5f8218c5782a587d75cf6
KietPhamVoAnh <kietpva0102@gmail.com>
2022-11-15 14:31:49 +0700

    Create Dart project

---
+++ dart_training/bin/dart_training.dart
@@ -0,0 +1,5 @@
+import 'package:dart_training/dart_training.dart' as dart_training;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_training.calculate()}!');
+}
---
+++ dart_training/test/dart_training_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_training/dart_training.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
