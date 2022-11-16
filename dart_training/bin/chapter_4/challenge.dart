13b3d9b076173b8e87dc2a25f1ce6e41f4bad24d
KietPhamVoAnh <kietpva0102@gmail.com>
2022-11-16 17:04:31 +0700

    challenge

---
+++ dart_training/bin/chapter_4/challenge.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  /**
+   * What’s wrong with the following code?
+   */
+  // const firstName = 'Bob';
+  // if (firstName == 'Bob') {
+  //   const lastName = 'Smith';
+  // } else if (firstName == 'Ray') {
+  //   const lastName = 'Wenderlich';
+  // }
+  // final fullName = firstName + ' ' + lastName;
+
+  /**
+   * Last Name is inside {}, so it can't be used outside {}
+   */
+}
