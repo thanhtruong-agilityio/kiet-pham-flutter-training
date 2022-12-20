f3b3c07803e6551def0383bd8929fdf05b019434
Kiet Pham <kietpva0102@gmail.com>
2022-12-20 15:35:45 +0700

    Create your own package - part 2

--- flutter_traning/chapter_10/packages/area/lib/area.dart
+++ flutter_traning/chapter_10/packages/area/lib/area.dart
@@ -2,14 +2,5 @@ library area;
 
 import 'package:intl/intl.dart';
 
-String calculateAreaRect(double width, double height) {
-  double result = width * height;
-  final formatter = NumberFormat("#.###");
-  return formatter.format(result);
-}
-
-String calculatedAreaTriangle(double width, double height) {
-  double result = width * height / 2;
-  final formatter = NumberFormat("#.####");
-  return formatter.format(result);
-}
+part 'rectangle.dart';
+part 'triangle.dart';
