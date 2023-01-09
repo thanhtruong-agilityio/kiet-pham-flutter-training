5159945a7615b2d97ed4f3596e31abc3427f2130
Kiet Pham <kietpva0102@gmail.com>
2023-01-09 11:01:05 +0700

    Setup: assets

---
+++ gotour_app/lib/gen/colors.gen.dart
@@ -0,0 +1,60 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #000000
+  static const Color backgroundColor = Color(0xFF000000);
+
+  /// Color: #FEEBDC
+  static const Color checkboxColor = Color(0xFFFEEBDC);
+
+  /// Color: #FF0000
+  static const Color errorColor = Color(0xFFFF0000);
+
+  /// Color: #CCCCCC
+  static const Color greyLightColor = Color(0xFFCCCCCC);
+
+  /// Color: #AAAAAA
+  static const Color iconsColor = Color(0xFFAAAAAA);
+
+  /// Color: #FFFFFF
+  static const Color onBackgroundColor = Color(0xFFFFFFFF);
+
+  /// Color: #FFFFFF
+  static const Color onErrorColor = Color(0xFFFFFFFF);
+
+  /// Color: #FFFFFF
+  static const Color onPrimaryColor = Color(0xFFFFFFFF);
+
+  /// Color: #FFFFFF
+  static const Color onSecondaryColor = Color(0xFFFFFFFF);
+
+  /// Color: #F8F8F8
+  static const Color onSurfaceColor = Color(0xFFF8F8F8);
+
+  /// Color: #FF7D0D
+  static const Color primaryColor = Color(0xFFFF7D0D);
+
+  /// Color: #888888
+  static const Color secondaryColor = Color(0xFF888888);
+
+  /// Color: #C4C4C4
+  static const Color sliderColor = Color(0xFFC4C4C4);
+
+  /// Color: #F8F8F8
+  static const Color surfaceColor = Color(0xFFF8F8F8);
+
+  /// Color: #666666
+  static const Color titleOfTextfieldColor = Color(0xFF666666);
+}
