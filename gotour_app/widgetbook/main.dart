ba252c26417882ed7ce92080478f4b5553e64785
Kiet Pham <kietpva0102@gmail.com>
2023-01-11 16:43:03 +0700

    Widgetbook: add more devices

--- gotour_app/widgetbook/main.dart
+++ gotour_app/widgetbook/main.dart
@@ -10,7 +10,25 @@ class WidgetbookHotReload extends StatelessWidget {
   Widget build(BuildContext context) {
     return Widgetbook(
       devices: const [
+        // iOS
+        Apple.iPhone8,
+        Apple.iPhone8Plus,
+
+        Apple.iPhone11,
+        Apple.iPhone11Pro,
+        Apple.iPhone11ProMax,
+
+        Apple.iPhone12,
+        Apple.iPhone12Pro,
+        Apple.iPhone12ProMax,
+
         Apple.iPhone13Mini,
+        Apple.iPhone13,
+        Apple.iPhone13Pro,
+        Apple.iPhone13ProMax,
+
+        // Android
+        Samsung.s10,
         Samsung.s21ultra,
       ],
       appInfo: AppInfo(
