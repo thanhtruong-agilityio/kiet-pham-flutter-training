92016a21a4f3280f49bc8c09ce291ae6aa046b4a
Kiet Pham <kietpva0102@gmail.com>
2023-02-02 14:43:36 +0700

    Widget: GTGridViewHotPlace

---
+++ gotour_app/lib/widgets/gridview-hot-place.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import '../gen/colors.gen.dart';
+import 'GTTitle.dart';
+
+class GTGridViewHotPlace extends StatelessWidget {
+  const GTGridViewHotPlace({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: const [
+            GTTitle(title: 'Hot Place'),
+          ],
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        const Expanded(
+          child: BuildGridview(),
+        ),
+      ],
+    );
+  }
+}
+
+class BuildGridview extends StatelessWidget {
+  const BuildGridview({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> dataImages = [
+      'assets/images/Kyoto.png',
+      'assets/images/ChumPhon.png',
+      'assets/images/Tibidabo.png',
+      'assets/images/Krabi.png',
+      'assets/images/PhraNang.png',
+      'assets/images/DoiPui.png',
+    ];
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+      ),
+      itemCount: dataImages.length,
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () {
+          print('tap item: $index');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            // color: ColorName.primaryColor,
+            image: DecorationImage(
+              image: AssetImage(
+                dataImages[index],
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
