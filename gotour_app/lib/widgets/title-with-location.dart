b492fd29f66860264b2ce1b983b9e107c241ee81
KietPham <kietpva0102@gmail.com>
2023-02-05 23:51:34 +0700

    Update: title-with-location.dart

--- gotour_app/lib/widgets/title-with-location.dart
+++ gotour_app/lib/widgets/title-with-location.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gotour_app/core/resources/assets_generated/assets.gen.dart';
-import 'package:gotour_app/core/resources/assets_generated/colors.gen.dart';
+import 'text.dart';
 
 class GTTitleWithLocation extends StatelessWidget {
   const GTTitleWithLocation({
     Key? key,
-    required this.namePlace,
+    required this.placeName,
     required this.location,
+    this.colorIcon,
+    this.colorName,
+    this.colorLocation,
   }) : super(key: key);
 
-  final String namePlace, location;
+  final String placeName, location;
+  final Color? colorIcon, colorName, colorLocation;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          namePlace,
-          style: Theme.of(context).textTheme.titleSmall,
+        GTText.PlaceName(
+          context,
+          placeName,
+          color: colorName,
         ),
         const SizedBox(
           height: 10,
@@ -28,18 +33,17 @@ class GTTitleWithLocation extends StatelessWidget {
           children: [
             SvgPicture.asset(
               Assets.icons.location,
-              color: ColorName.primaryColor,
+              color: colorIcon,
               width: 10,
               height: 12,
             ),
             const SizedBox(
               width: 10,
             ),
-            Text(
+            GTText.PlaceLocation(
+              context,
               location,
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: ColorName.iconsColor,
-                  ),
+              color: colorLocation,
             ),
           ],
         ),
