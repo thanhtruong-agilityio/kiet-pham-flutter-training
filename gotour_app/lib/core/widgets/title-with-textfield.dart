9e1f62f05ec1d0976e4f0098ad0c6e19cd4d47e0
Kiet Pham <kietpva0102@gmail.com>
2023-02-08 15:36:01 +0700

    Fix: TextField

--- gotour_app/lib/core/widgets/title-with-textfield.dart
+++ gotour_app/lib/core/widgets/title-with-textfield.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gotour_app/core/resources/assets_generated/colors.gen.dart';
 
-import 'package:gotour_app/core/resources/assets_generated/fonts.gen.dart';
 import 'text.dart';
 
 class GTTextField extends StatefulWidget {
   const GTTextField({
     super.key,
-    // required this.obscureText,
     required this.hintText,
     required this.title,
     required this.hideText,
@@ -41,7 +39,7 @@ class _GTTextFieldState extends State<GTTextField> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          GTText.labelMedium(
+          GTText.labelLarge(
             context,
             widget.title,
             color: ColorName.titleOfTextfieldColor,
@@ -49,61 +47,37 @@ class _GTTextFieldState extends State<GTTextField> {
           const SizedBox(
             height: 5,
           ),
-          TextField(
-            style: Theme.of(context)
-                .textTheme
-                .labelSmall!
-                .copyWith(fontFamily: FontFamily.gilroyMedium, height: 1.214),
-            cursorColor: ColorName.textColor,
-            obscureText: _passWis,
-            decoration: InputDecoration(
-              suffixIcon: widget.hideText
-                  ? GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _passWis = !_passWis;
-                        });
-                      },
-                      child: _passWis
-                          ? const Icon(
-                              Icons.visibility_off,
-                              color: ColorName.textColor,
-                            )
-                          : const Icon(
-                              Icons.visibility,
-                              color: ColorName.textColor,
-                            ),
-                    )
-                  : null,
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: ColorName.borderRadiusColor,
-                ),
-                borderRadius: BorderRadius.circular(7),
+          SizedBox(
+            height: 40,
+            child: TextField(
+              textAlignVertical: TextAlignVertical.bottom,
+              // textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.labelMedium,
+              cursorColor: ColorName.textColor,
+              obscureText: _passWis,
+              decoration: InputDecoration(
+                suffixIcon: widget.hideText
+                    ? GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _passWis = !_passWis;
+                          });
+                        },
+                        child: _passWis
+                            ? const Icon(
+                                Icons.visibility_off,
+                                color: ColorName.textColor,
+                              )
+                            : const Icon(
+                                Icons.visibility,
+                                color: ColorName.textColor,
+                              ),
+                      )
+                    : null,
+                hintText: widget.hintText,
               ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: ColorName.borderRadiusColor,
-                ),
-                borderRadius: BorderRadius.circular(7),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: ColorName.errorColor,
-                ),
-                borderRadius: BorderRadius.circular(7),
-              ),
-              hintText: widget.hintText,
-              hintStyle: Theme.of(context).textTheme.labelSmall!.copyWith(
-                    fontFamily: FontFamily.gilroyMedium,
-                    height: 1.214,
-                    color: ColorName.iconsColor,
-                  ),
+              // onChanged: widget.press as Function(String),
             ),
-            // onChanged: widget.press as Function(String),
           ),
         ],
       ),
