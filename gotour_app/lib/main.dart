edfedf7352f27d59bfea6ee075d5a4610d6a7372
KietPham <kietpva0102@gmail.com>
2023-03-06 19:02:23 +0700

    Setup: firebase

--- gotour_app/lib/main.dart
+++ gotour_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -11,10 +12,15 @@ import 'package:gotour_app/features/hot_place/hot_place_page.dart';
 import 'package:gotour_app/features/main/main_page.dart';
 import 'package:gotour_app/features/misc/onboarding_page.dart';
 import 'package:gotour_app/features/tour_details/tour_details_page.dart';
+import 'package:gotour_app/firebase_options.dart';
 
-void main() {
+void main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
