c4f0cb6578effe05007dea8df411c84d1045ae22
Kiet Pham <kietpva0102@gmail.com>
2023-03-07 17:06:18 +0700

    Fix: determineError

--- gotour_app/lib/features/auth/repository/auth_repository.dart
+++ gotour_app/lib/features/auth/repository/auth_repository.dart
@@ -56,43 +56,51 @@ class AuthRepository {
     try {
       await _firebaseAuth.signOut();
     } on Exception catch (e) {
-      throw Exception(e.toString());
+      // throw Exception(e.toString());
+      throw Exception(e);
     }
   }
 
-  // AuthError _determineError(FirebaseAuthException exception) {
-  //   switch (exception.code) {
-  //     case 'invalid-email':
-  //       return AuthError.invalidEmail;
-  //     case 'user-disabled':
-  //       return AuthError.userDisabled;
-  //     case 'user-not-found':
-  //       return AuthError.userNotFound;
-  //     case 'wrong-password':
-  //       return AuthError.wrongPassword;
-  //     case 'email-already-in-use':
-  //     case 'account-exists-with-different-credential':
-  //       return AuthError.emailAlreadyInUse;
-  //     case 'invalid-credential':
-  //       return AuthError.invalidCredential;
-  //     case 'operation-not-allowed':
-  //       return AuthError.operationNotAllowed;
-  //     case 'weak-password':
-  //       return AuthError.weakPassword;
-  //     case 'ERROR_MISSING_GOOGLE_AUTH_TOKEN':
-  //     default:
-  //       return AuthError.error;
-  //   }
-  // }
+  AuthError _determineError(FirebaseAuthException exception) {
+    switch (exception.code) {
+      case 'invalid-email':
+        return AuthError.invalidEmail;
+      case 'user-disabled':
+        return AuthError.userDisabled;
+      case 'user-not-found':
+        return AuthError.userNotFound;
+      case 'wrong-password':
+        return AuthError.wrongPassword;
+      case 'email-already-in-use':
+        return AuthError.emailAlreadyInUse;
+      case 'account-exists-with-different-credential':
+        return AuthError.emailAlreadyInUse;
+      case 'invalid-credential':
+        return AuthError.invalidCredential;
+      case 'operation-not-allowed':
+        return AuthError.operationNotAllowed;
+      case 'weak-password':
+        return AuthError.weakPassword;
+      case 'ERROR_MISSING_GOOGLE_AUTH_TOKEN':
+        return AuthError.missingGoogleAuthToken;
+      case 'network-request-failed':
+        return AuthError.networkError;
+      default:
+        return AuthError.error;
+    }
+  }
 }
-// enum AuthError {
-//   invalidEmail,
-//   userDisabled,
-//   userNotFound,
-//   wrongPassword,
-//   emailAlreadyInUse,
-//   invalidCredential,
-//   operationNotAllowed,
-//   weakPassword,
-//   error,
-// }
+
+enum AuthError {
+  invalidEmail,
+  userDisabled,
+  userNotFound,
+  wrongPassword,
+  emailAlreadyInUse,
+  invalidCredential,
+  operationNotAllowed,
+  weakPassword,
+  networkError,
+  missingGoogleAuthToken,
+  error,
+}
