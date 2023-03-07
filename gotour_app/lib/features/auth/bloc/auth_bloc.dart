a011f4baa9f4fb1d1a31f6ac8655a19e36f092b8
KietPham <kietpva0102@gmail.com>
2023-03-07 22:04:45 +0700

    Fix: comment

--- gotour_app/lib/features/auth/bloc/auth_bloc.dart
+++ gotour_app/lib/features/auth/bloc/auth_bloc.dart
@@ -9,12 +9,21 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc({required this.authRepository}) : super(UnAuthenticated()) {
+    // When User Presses the SignIn Button, we will send
+    // the SignInRequested Event to the AuthBloc to handle it and
+    // emit the Authenticated State if the user is authenticated
     on<SignInRequested>(_handleSignInRequested);
-    // When User Presses the SignUp Button, we will send the SignUpRequest Event to the AuthBloc to handle it and emit the Authenticated State if the user is authenticated
+    // When User Presses the SignUp Button, we will send
+    //the SignUpRequest Event to the AuthBloc to handle it and
+    // emit the Authenticated State if the user is authenticated
     on<SignUpRequested>(_handleSignUpRequested);
-    // When User Presses the Google Login Button, we will send the GoogleSignInRequest Event to the AuthBloc to handle it and emit the Authenticated State if the user is authenticated
+    // When User Presses the Google Login Button, we will send
+    // the GoogleSignInRequest Event to the AuthBloc to handle it and
+    // emit the Authenticated State if the user is authenticated
     on<GoogleSignInRequested>(_handleGoogleSignInRequested);
-    // When User Presses the SignOut Button, we will send the SignOutRequested Event to the AuthBloc to handle it and emit the UnAuthenticated State
+    // When User Presses the SignOut Button, we will send
+    // the SignOutRequested Event to the AuthBloc to handle it and
+    // emit the UnAuthenticated State
     on<SignOutRequested>(_handleSignOutRequested);
   }
 
