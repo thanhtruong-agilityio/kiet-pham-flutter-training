5fdfc0f4d130b0dae4cd2a31c514a2695979b20a
KietPham <kietpva0102@gmail.com>
2023-03-15 21:14:28 +0700

    Create: main repository

--- gotour_app/lib/features/main/repository/main_repository.dart
+++ gotour_app/lib/features/main/repository/main_repository.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gotour_app/features/main/models/model_hot_place.dart';
+import 'package:gotour_app/features/main/models/model_id_tour.dart';
 import 'package:gotour_app/features/main/models/model_my_location.dart';
 
 class MainRepository {
   final _firebaseFirestoreHotPlace =
       FirebaseFirestore.instance.collection('tour-details');
-  final _firebaseFirestoreMyLocation =
+  final _firebaseFirestoreTourBookMarks =
       FirebaseFirestore.instance.collection('book-marks');
 
   Future<List<HotPlace>> fetchDataHotPlace() async {
@@ -22,5 +23,42 @@ class MainRepository {
           ),
         )
         .toList();
+  }
+
+  Future<List<IdTour>> fetchListTourBookmarkByUser({
+    required String idUser,
+  }) async {
+    final data = await _firebaseFirestoreTourBookMarks
+        .where('idUser', isEqualTo: idUser)
+        .get();
+    return (data.docs)
+        .map((tour) => IdTour(idTour: tour['idTour'] as String))
+        .toList();
+  }
+
+  Future<List<MyLocation>> getDataFromDocuments({
+    required List<String> documentIds,
+  }) async {
+    if (documentIds.isNotEmpty) {
+      final QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('tour-details')
+          .where(FieldPath.documentId, whereIn: documentIds)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        final dataList = querySnapshot.docs
+            .map<MyLocation>(
+              (json) => MyLocation(
+                descriptions: json['descriptions'] as String,
+                imageUrl: json['imageUrl'] as String,
+                location: json['location'] as String,
+                placeName: json['placeName'] as String,
+              ),
+            )
+            .toList();
+        return dataList;
+      }
+    }
+    return [];
   }
 }
