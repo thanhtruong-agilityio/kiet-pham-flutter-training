344af2761b1fc3ed30d9b4642692e1692aaa5118
KietPham <kietpva0102@gmail.com>
2023-03-20 14:11:08 +0700

    Create: model my location

---
+++ gotour_app/lib/features/main/models/model_my_location.dart
@@ -0,0 +1,29 @@
+class MyLocation {
+  MyLocation({
+    // required this.id,
+    required this.descriptions,
+    required this.imageUrl,
+    required this.location,
+    required this.placeName,
+  });
+
+  factory MyLocation.fromJson(Map<String, dynamic> json) => MyLocation(
+        // id: json['id'] as String,
+        descriptions: json['descriptions'] as String,
+        imageUrl: json['imageUrl'] as String,
+        location: json['location'] as String,
+        placeName: json['placeName'] as String,
+      );
+  // String id;
+  String descriptions;
+  String imageUrl;
+  String location;
+  String placeName;
+
+  Map<String, dynamic> toJson() => {
+        'descriptions': descriptions,
+        'imageUrl': imageUrl,
+        'location': location,
+        'placeName': placeName,
+      };
+}
