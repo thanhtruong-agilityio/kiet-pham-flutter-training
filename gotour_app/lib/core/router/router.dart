2b3d11f384e3370886629268100b2a2846a1e6c5
KietPham <kietpva0102@gmail.com>
2023-03-20 14:21:01 +0700

    Update: ShellRoute

--- gotour_app/lib/core/router/router.dart
+++ gotour_app/lib/core/router/router.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:gotour_app/core/widgets/bottom_navigation_bar.dart';
 import 'package:gotour_app/features/auth/forgot_password/forgot_password_page.dart';
 import 'package:gotour_app/features/auth/login/login_page.dart';
 import 'package:gotour_app/features/auth/sign_up/sign_up.dart';
@@ -11,47 +12,52 @@ import 'package:gotour_app/features/tour_details/tour_details_page.dart';
 
 final GoRouter router = GoRouter(
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData &&
-              FirebaseAuth.instance.currentUser!.emailVerified == true) {
-            return const GTMainPage();
-          }
-          return const GTOnboardingScreen();
-        },
-      ),
-    ),
-    GoRoute(
-      path: '/login-page',
-      builder: (context, state) => const GTLoginPage(),
-    ),
-    GoRoute(
-      path: '/forgot-password-page',
-      builder: (context, state) => const GTForgotPasswordPage(),
-    ),
-    GoRoute(
-      path: '/sign-up-page',
-      builder: (context, state) => const GTSignUpPage(),
-    ),
-    GoRoute(
-      name: 'main',
-      path: '/main-page',
-      builder: (context, state) => const GTMainPage(),
+    ShellRoute(
       routes: [
         GoRoute(
-          name: 'tour-details',
-          path: 'tour-details',
-          builder: (context, state) => const GTTourDetails(),
+          path: '/',
+          builder: (context, state) => StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData &&
+                  FirebaseAuth.instance.currentUser!.emailVerified == true) {
+                return const GTMainPage();
+              }
+              return const GTOnboardingScreen();
+            },
+          ),
         ),
         GoRoute(
-          name: 'hot-place',
-          path: 'hot-place',
-          builder: (context, state) => const GTHotPlace(),
+          path: '/login-page',
+          builder: (context, state) => const GTLoginPage(),
+        ),
+        GoRoute(
+          path: '/forgot-password-page',
+          builder: (context, state) => const GTForgotPasswordPage(),
+        ),
+        GoRoute(
+          path: '/sign-up-page',
+          builder: (context, state) => const GTSignUpPage(),
+        ),
+        GoRoute(
+          name: 'main',
+          path: '/main-page',
+          builder: (context, state) => const GTMainPage(),
+          routes: [
+            GoRoute(
+              name: 'tour-details',
+              path: 'tour-details',
+              builder: (context, state) => const GTTourDetails(),
+            ),
+            GoRoute(
+              name: 'hot-place',
+              path: 'hot-place',
+              builder: (context, state) => const GTHotPlace(),
+            ),
+          ],
         ),
       ],
+      builder: (context, state, child) => const GTBottomNavigationBar(),
     ),
   ],
 );
