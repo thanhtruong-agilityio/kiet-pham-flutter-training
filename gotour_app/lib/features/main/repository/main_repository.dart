51cc259473a28b951ca67d9b61049128b59d8cb8
KietPham <kietpva0102@gmail.com>
2023-03-20 14:13:06 +0700

    Create: main repository

---
+++ gotour_app/lib/features/main/repository/main_repository.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gotour_app/features/main/models/model_best_place.dart';
+import 'package:gotour_app/features/main/models/model_id_tour.dart';
+import 'package:gotour_app/features/main/models/model_my_location.dart';
+
+class MainRepository {
+  final _firebaseFirestoreBestPlace =
+      FirebaseFirestore.instance.collection('tour-details');
+
+  final _firebaseFirestoreTourBookMarks =
+      FirebaseFirestore.instance.collection('book-marks');
+
+  // Fetch Data Best Place from the firebase
+  Future<List<BestPlace>> fetchDataBestPlace() async {
+    final data = await _firebaseFirestoreBestPlace
+        .where('hotPlace', isEqualTo: true)
+        .get();
+    return (data.docs)
+        .map(
+          (tour) => BestPlace(
+            imageUrl: tour['imageUrl'] as String,
+            location: tour['location'] as String,
+            placeName: tour['placeName'] as String,
+            price: tour['price'] as String,
+          ),
+        )
+        .toList();
+  }
+
+  // Fetch List Tour from the firebase
+  Future<List<IdTour>> fetchListTourBookmarkByUser({
+    required String idUser,
+  }) async {
+    final data = await _firebaseFirestoreTourBookMarks
+        .where('idUser', isEqualTo: idUser)
+        .get();
+    return (data.docs)
+        .map((tour) => IdTour(idTour: tour['idTour'] as String))
+        .toList();
+  }
+
+  // Fetch My Location Place from the firebase
+  Future<List<MyLocation>> getDataFromDocuments({
+    required List<String> documentIds,
+  }) async {
+    if (documentIds.isNotEmpty) {
+      final QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('tour-details')
+          .where(FieldPath.documentId, whereIn: documentIds)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        final dataList = querySnapshot.docs
+            .map<MyLocation>(
+              (json) => MyLocation(
+                descriptions: json['descriptions'] as String,
+                imageUrl: json['imageUrl'] as String,
+                location: json['location'] as String,
+                placeName: json['placeName'] as String,
+              ),
+            )
+            .toList();
+        return dataList;
+      }
+    }
+    return [];
+  }
+}
