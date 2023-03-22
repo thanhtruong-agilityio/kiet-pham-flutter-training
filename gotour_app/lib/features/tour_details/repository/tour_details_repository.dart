cbfbebff7cd385f2ec2a6ca4ba6b7b15f3f4f487
KietPham <kietpva0102@gmail.com>
2023-03-22 15:54:36 +0700

    Create: tour details repository

---
+++ gotour_app/lib/features/tour_details/repository/tour_details_repository.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gotour_app/features/tour_details/model/model_tour_details.dart';
+
+class TourDetailsRepository {
+  final _firebaseFirestoreTourDetails =
+      FirebaseFirestore.instance.collection('tour-details');
+
+  Future<TourDetails> fetchDataTour({
+    required String idTour,
+  }) async {
+    final data = await _firebaseFirestoreTourDetails.doc(idTour).get();
+    return TourDetails(
+      id: data['id'] as String,
+      imageUrl: data['imageUrl'] as String,
+      location: data['location'] as String,
+      placeName: data['placeName'] as String,
+      price: data['price'] as String,
+      descriptions: data['descriptions'] as String,
+      weather: data['weather'] as String,
+    );
+  }
+}
