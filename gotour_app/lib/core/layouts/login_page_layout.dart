fa794bf211c1b96f0d9cb422cbcec47febf9ed7a
KietPham <kietpva0102@gmail.com>
2023-03-25 15:25:30 +0700

    Create: login page layout

---
+++ gotour_app/lib/core/layouts/login_page_layout.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+
+class GTLoginLayout extends StatelessWidget {
+  const GTLoginLayout({
+    super.key,
+    required this.logo,
+    required this.title,
+    required this.body,
+  });
+
+  final Widget logo;
+  final Widget title;
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 76),
+                child: Column(
+                  children: [
+                    logo,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 72),
+                      child: title,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 47,
+                  left: 20,
+                  right: 20,
+                  bottom: 38,
+                ),
+                child: body,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
