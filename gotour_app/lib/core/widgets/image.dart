8ccb5d627894d0e52457f4604f81f26dfd568dff
KietPham <kietpva0102@gmail.com>
2023-03-26 09:32:22 +0700

    Fix: widget GTImage

--- gotour_app/lib/core/widgets/image.dart
+++ gotour_app/lib/core/widgets/image.dart
@@ -4,15 +4,21 @@ class GTImage extends StatelessWidget {
   const GTImage({
     super.key,
     required this.images,
+    required this.height,
+    required this.width,
   });
 
   final String images;
+  final double height;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: height,
+      width: width,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(10),
         image: DecorationImage(
           image: AssetImage(images),
           fit: BoxFit.cover,
