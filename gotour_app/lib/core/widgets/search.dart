8cb7d1adf11abc6ee8633e1b9044eae1222d6042
KietPham <kietpva0102@gmail.com>
2023-03-26 09:32:47 +0700

    Create: search widget

---
+++ gotour_app/lib/core/widgets/search.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:gotour_app/core/resources/assets_generated/assets.gen.dart';
+import 'package:gotour_app/core/resources/l10n_generated/l10n.dart';
+import 'package:gotour_app/core/widgets/button.dart';
+
+class GTSearch extends StatelessWidget {
+  const GTSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: 40,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: S.of(context).mainPageHintTextSearchButton,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          GTIconButton(
+            icon: Assets.icons.search,
+            iconColor: Theme.of(context).colorScheme.background,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
