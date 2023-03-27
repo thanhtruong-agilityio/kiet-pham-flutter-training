fa4da63f7b5aab141e7195cfbd6a5986055cad29
KietPham <kietpva0102@gmail.com>
2023-03-27 13:57:43 +0700

    Move: assets from mobile_app to gotour_ui & use fluttergen

---
+++ gotour_app/apps/gotour_ui/lib/core/resources/assets_generated/colors.gen.dart
@@ -0,0 +1,72 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #FFFFFF
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+
+  /// Color: #FF0000
+  static const Color errorColor = Color(0xFFFF0000);
+
+  /// Color: #FFD1AA
+  static const Color inversePrimaryColor = Color(0xFFFFD1AA);
+
+  /// Color: #000000
+  static const Color onBackgroundColor = Color(0xFF000000);
+
+  /// Color: #FFFFFF
+  static const Color onErrorColor = Color(0xFFFFFFFF);
+
+  /// Color: #FFFFFF
+  static const Color onPrimaryColor = Color(0xFFFFFFFF);
+
+  /// Color: #FFFFFF
+  static const Color onSecondaryColor = Color(0xFFFFFFFF);
+
+  /// Color: #CCCCCC
+  static const Color onSecondaryContainerColor = Color(0xFFCCCCCC);
+
+  /// Color: #000000
+  static const Color onSurfaceColor = Color(0xFF000000);
+
+  /// Color: #EEEEEE
+  static const Color onSurfaceVariant = Color(0xFFEEEEEE);
+
+  /// Color: #333333
+  static const Color outlineColor = Color(0xFF333333);
+
+  /// Color: #0000000d
+  static const Color outlineVariantColor = Color(0x0000000D);
+
+  /// Color: #FF7D0D
+  static const Color primaryColor = Color(0xFFFF7D0D);
+
+  /// Color: #888888
+  static const Color secondaryColor = Color(0xFF888888);
+
+  /// Color: #17E364
+  static const Color secondaryContainerColor = Color(0xFF17E364);
+
+  /// Color: #F8F8F8
+  static const Color surfaceColor = Color(0xFFF8F8F8);
+
+  /// Color: #666666
+  static const Color surfaceTintColor = Color(0xFF666666);
+
+  /// Color: #AAAAAA
+  static const Color tertiaryColor = Color(0xFFAAAAAA);
+
+  /// Color: #C4C4C4
+  static const Color tertiaryContainerColor = Color(0xFFC4C4C4);
+}
