88cba85d4ec3fc7f738a7079af7da0364713caef
KietPham <kietpva0102@gmail.com>
2023-03-30 10:57:03 +0700

    Handle: when user click any card best place, go to hot place page

--- mobile_app/apps/gotour_app/lib/features/hot_place/hot_place_page.dart
+++ mobile_app/apps/gotour_app/lib/features/hot_place/hot_place_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gotour_app/core/assets/assets.dart';
 import 'package:gotour_app/core/shared/device_info.dart';
+import 'package:gotour_app/features/hot_place/bloc/hot_place_bloc.dart';
 import 'package:gotour_app/features/hot_place/hot_place.dart';
 import 'package:gotour_app/features/hot_place/place_info.dart';
-import 'package:gotour_ui/core/resources/assets_generated/assets.gen.dart';
+import 'package:gotour_app/features/hot_place/repository/hot_place_repository.dart';
+import 'package:gotour_ui/core/shared/snack_bar.dart';
 import 'package:gotour_ui/core/widgets/app_bar.dart';
 import 'package:gotour_ui/core/widgets/button.dart';
 import 'package:gotour_ui/core/widgets/scaffold.dart';
+import 'package:gotour_ui/core/widgets/text.dart';
 
 class GTHotPlace extends StatelessWidget {
   const GTHotPlace({
@@ -45,22 +49,86 @@ class GTHotPlace extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: device.scale(20)),
-        child: Column(
-          children: [
-            SizedBox(height: device.scale(44)),
-            GTPlaceInfoHotPlace(
-              image: Assets.images.tibidabo.path,
-              placeName: 'Capital of Thailand',
-              location: 'Bangkok, Thailand',
-              price: '5 000',
-              pressCard: () {},
-              pressBtn: () {},
+      body: RepositoryProvider(
+        create: (context) => HotPlaceRepository(),
+        child: BlocProvider(
+          create: (context) => HotPlaceBloc(
+            hotPlaceRepository:
+                RepositoryProvider.of<HotPlaceRepository>(context),
+          ),
+          child: BlocListener<HotPlaceBloc, HotPlaceState>(
+            listener: (context, state) {
+              if (state is ChangeBookmarkHotPlaceSuccessState) {
+                if (state.bookmark == true) {
+                  GTSnackBar.show(
+                    context,
+                    message: 'Bookmark tour success',
+                    backgroundColor: colorScheme.secondaryContainer,
+                  );
+                } else {
+                  GTSnackBar.show(
+                    context,
+                    message: 'Un Bookmark tour success',
+                    backgroundColor: colorScheme.secondaryContainer,
+                  );
+                }
+              }
+            },
+            child: BlocBuilder<HotPlaceBloc, HotPlaceState>(
+              builder: (context, state) {
+                if (state is HotPlaceInitialState) {
+                  context
+                      .read<HotPlaceBloc>()
+                      .add(HotPlaceFetchDataEvent(tourId: id));
+                }
+                if (state is HotPlaceLoadingState) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: colorScheme.primary,
+                    ),
+                  );
+                }
+                if (state is HotPlaceLoadedState) {
+                  final data = state.hotPlace;
+                  final isBookMark = state.isBookMark;
+
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: device.scale(20)),
+                    child: Column(
+                      children: [
+                        SizedBox(height: device.scale(44)),
+                        GTPlaceInfoHotPlace(
+                          image: data.imageUrl,
+                          placeName: data.placeName,
+                          location: data.location,
+                          price: data.price,
+                          tagList: data.tagList,
+                          isBookmark: isBookMark,
+                          pressBtn: () {},
+                          pressCard: () {},
+                          onBookmark: () {
+                            context.read<HotPlaceBloc>().add(
+                                  PressTheBookmarkButtonEvent(
+                                    isBookmark: isBookMark,
+                                    tourId: id,
+                                  ),
+                                );
+                          },
+                        ),
+                        SizedBox(height: device.scale(25)),
+                        Expanded(
+                          child: GTGridViewHotPlace(
+                            imageList: data.imageList,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return Center(child: GTText.bodyLarge(context, text: 'fail'));
+              },
             ),
-            SizedBox(height: device.scale(25)),
-            const Expanded(child: GTGridViewHotPlace()),
-          ],
+          ),
         ),
       ),
     );
