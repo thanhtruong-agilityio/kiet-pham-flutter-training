ce5d59994cee36a7921d8e4dc88873042edb5686
KietPham <kietpva0102@gmail.com>
2023-03-31 16:29:05 +0700

    Add: model icon

---
+++ mobile_app/apps/gotour_widgetbook/lib/core/models/icon.dart
@@ -0,0 +1,37 @@
+import 'package:gotour_widgetbook/core/assets/assets.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+final icons = <Option<String>>[
+  Option(
+    label: 'back',
+    value: GTWidgetbookAssets().back,
+  ),
+  Option(
+    label: 'bell',
+    value: GTWidgetbookAssets().bell,
+  ),
+  Option(
+    label: 'boat',
+    value: GTWidgetbookAssets().boat,
+  ),
+  Option(
+    label: 'bookMark',
+    value: GTWidgetbookAssets().bookMark,
+  ),
+  Option(
+    label: 'car',
+    value: GTWidgetbookAssets().car,
+  ),
+  Option(
+    label: 'checkmark',
+    value: GTWidgetbookAssets().checkmark,
+  ),
+  Option(
+    label: 'cloud',
+    value: GTWidgetbookAssets().cloud,
+  ),
+  Option(
+    label: 'google',
+    value: GTWidgetbookAssets().google,
+  ),
+];
