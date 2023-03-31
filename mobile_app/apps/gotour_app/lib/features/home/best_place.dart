7954ab06e19640c28a34a8acc963f96116fd385b
KietPham <kietpva0102@gmail.com>
2023-03-31 11:06:26 +0700

    Fix: remove handle bloc in best place

--- mobile_app/apps/gotour_app/lib/features/home/best_place.dart
+++ mobile_app/apps/gotour_app/lib/features/home/best_place.dart
@@ -1,92 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gotour_app/core/shared/device_info.dart';
-import 'package:gotour_app/features/home/bloc/home_bloc.dart';
 import 'package:gotour_app/features/home/models/best_place.dart';
-import 'package:gotour_app/features/home/repository/home_repository.dart';
-import 'package:gotour_ui/core/resources/l10n_generated/l10n.dart';
 import 'package:gotour_ui/core/widgets/button.dart';
 import 'package:gotour_ui/core/widgets/location.dart';
-import 'package:gotour_ui/core/widgets/text.dart';
-import 'package:gotour_ui/core/widgets/title.dart';
 
 class GTBestPlace extends StatelessWidget {
   const GTBestPlace({
     super.key,
+    required this.bestPlaceList,
   });
+
+  final List<BestPlace> bestPlaceList;
 
   @override
   Widget build(BuildContext context) {
-    var data = <BestPlace>[];
+    final data = bestPlaceList;
 
     final device = GTReponsive.of(context);
-    return RepositoryProvider(
-      create: (context) => HomeRepository(),
-      child: BlocProvider(
-        create: (context) => HomeBloc(
-          mainRepository: RepositoryProvider.of<HomeRepository>(context),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.only(right: device.scale(20)),
-              margin: EdgeInsets.only(bottom: device.scale(14)),
-              child: Padding(
-                padding: EdgeInsets.only(left: device.scale(20)),
-                child: GTTitle(
-                  title: S.of(context).mainPageBestPlace,
-                  onPressed: () => context.goNamed('best-place'),
+    return Row(
+      children: [
+        SizedBox(
+          height: device.scale(180),
+          width: device.scale(375),
+          child: ListView.builder(
+            padding: EdgeInsets.only(left: device.scale(20)),
+            scrollDirection: Axis.horizontal,
+            itemCount: data.length,
+            itemBuilder: (context, index) => Container(
+              margin: EdgeInsets.only(right: device.scale(20)),
+              child: GTCardBestPlace(
+                image: data[index].imageUrl,
+                placeName: data[index].placeName,
+                location: data[index].location,
+                price: data[index].price,
+                pressCard: () => context.goNamed(
+                  'hot-place',
+                  params: {'id': data[index].id},
                 ),
+                pressBtnPrice: () {},
               ),
             ),
-            Row(
-              children: [
-                SizedBox(
-                  height: device.scale(180),
-                  width: device.scale(375),
-                  child: BlocBuilder<HomeBloc, HomeState>(
-                    builder: (context, state) {
-                      if (state is HomeInitialState) {
-                        context.read<HomeBloc>().add(BestPlaceFetchDataEvent());
-                      }
-                      if (state is BestPlaceLoadingState) {
-                        return Center(
-                          child: GTText.bodyLarge(context, text: 'Loading'),
-                        );
-                      }
-                      if (state is BestPlaceLoadedState) {
-                        data = state.listBestPlace;
-                        return ListView.builder(
-                          padding: EdgeInsets.only(left: device.scale(20)),
-                          scrollDirection: Axis.horizontal,
-                          itemCount: data.length,
-                          itemBuilder: (context, index) => Container(
-                            margin: EdgeInsets.only(right: device.scale(20)),
-                            child: GTCardBestPlace(
-                              image: data[index].imageUrl,
-                              placeName: data[index].placeName,
-                              location: data[index].location,
-                              price: data[index].price,
-                              pressCard: () => context.goNamed(
-                                'hot-place',
-                                params: {'id': data[index].id},
-                              ),
-                              pressBtnPrice: () {},
-                            ),
-                          ),
-                        );
-                      }
-                      return Container();
-                    },
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
