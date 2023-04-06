f6492c811117bbe6565a2b1a3a504910b8543941
KietPham <kietpva0102@gmail.com>
2023-04-07 01:19:21 +0700

    Fix: main widgetbook

--- mobile_app/apps/gotour_widgetbook/lib/main.dart
+++ mobile_app/apps/gotour_widgetbook/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gotour_ui/core/theme/theme.dart';
-import 'package:gotour_ui/core/widgets/bottom_navigation_bar.dart';
-import 'package:gotour_widgetbook/core/models/color.dart';
-import 'package:gotour_widgetbook/core/widgets/card_color.dart';
+import 'package:gotour_widgetbook/core/widgets_component/bottom_navigation_bar.dart';
 import 'package:gotour_widgetbook/core/widgets_component/button.dart';
+import 'package:gotour_widgetbook/core/widgets_component/color.dart';
 import 'package:gotour_widgetbook/core/widgets_component/text.dart';
 import 'package:gotour_widgetbook/core/widgets_component/textfield.dart';
 import 'package:widgetbook/widgetbook.dart';
@@ -83,13 +82,7 @@ class _WidgetbookHotReloadState extends State<WidgetbookHotReload> {
               useCases: [
                 WidgetbookUseCase(
                   name: 'Bottom Navigation Bar',
-                  builder: (context) => const SafeArea(
-                      child: Scaffold(
-                    bottomNavigationBar: GTBottomNavigationBar(
-                      icons: [],
-                      routes: [],
-                    ),
-                  )),
+                  builder: (context) => const WBBottomNavigationBar(),
                 )
               ],
             ),
@@ -98,13 +91,7 @@ class _WidgetbookHotReloadState extends State<WidgetbookHotReload> {
               useCases: [
                 WidgetbookUseCase(
                   name: 'Color',
-                  builder: (context) => Column(
-                    children: [
-                      Expanded(flex: 6, child: CardColor(data: whiteToBlack)),
-                      Expanded(flex: 2, child: CardColor(data: primaryColor)),
-                      Expanded(flex: 2, child: CardColor(data: errorColor)),
-                    ],
-                  ),
+                  builder: (context) => const WBColor(),
                 ),
               ],
             )
