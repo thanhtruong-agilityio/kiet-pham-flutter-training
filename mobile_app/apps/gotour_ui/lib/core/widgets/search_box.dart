88de0cb6a3e8fafdf5b6abffa2bc6498ec62752c
KietPham <kietpva0102@gmail.com>
2023-04-15 21:28:36 +0700

    Fix search box widget

--- mobile_app/apps/gotour_ui/lib/core/widgets/search_box.dart
+++ mobile_app/apps/gotour_ui/lib/core/widgets/search_box.dart
@@ -3,8 +3,19 @@ import 'package:gotour_ui/core/assets.dart';
 import 'package:gotour_ui/core/resources/l10n_generated/l10n.dart';
 import 'package:gotour_ui/core/widgets/button.dart';
 
-class GTSearch extends StatelessWidget {
-  const GTSearch({super.key});
+class GTSearchBox extends StatelessWidget {
+  const GTSearchBox({
+    super.key,
+    this.onPressed,
+    this.value,
+    this.controller,
+    this.onClear,
+  });
+
+  final VoidCallback? onPressed;
+  final VoidCallback? onClear;
+  final void Function(String)? value;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +28,20 @@ class GTSearch extends StatelessWidget {
             child: SizedBox(
               height: 40,
               child: TextFormField(
+                controller: controller,
                 style: Theme.of(context).textTheme.labelMedium,
                 textAlignVertical: TextAlignVertical.bottom,
                 decoration: InputDecoration(
                   hintText: S.of(context).mainPageHintTextSearchButton,
+                  suffixIcon: GestureDetector(
+                    onTap: onClear ?? () {},
+                    child: const Icon(
+                      Icons.clear_outlined,
+                      size: 16,
+                    ),
+                  ),
                 ),
+                onChanged: value,
               ),
             ),
           ),
@@ -29,7 +49,7 @@ class GTSearch extends StatelessWidget {
           GTIconButton(
             icon: GTAssets.icSearch,
             iconColor: Theme.of(context).colorScheme.background,
-            onPressed: () {},
+            onPressed: onPressed ?? () {},
           ),
         ],
       ),
